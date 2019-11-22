00183127d3a93ecd9f454744dad5d85f1c53bac3
gernima <am.ra2016@yandex.ru>
2019-11-22 09:11:13 +0300

    create and update en, ru, lit; clean code

--- main.py
+++ main.py
@@ -8,17 +8,10 @@ from gtts import gTTS
 import subjects, talking, home_work, tests_subjects, gdz
 from yandex_translate import YandexTranslate
 
-subjects_tf = False
-audio = False
-ru_tf = False
-gdz_tf = False
-dz_tf = False
-tests_tf = False
-translate_tf = False
-en_tf = False
 token = '1049041175:AAFHw6FXE2-yCv7L4sJmwg50eImuAusJOG0'
 bot = telebot.TeleBot(token)
 chromedriver_path = '~/chromedriver'
+translate = YandexTranslate('trnsl.1.1.20191118T112631Z.f64a9d42a3ccc05f.2d0224570891cba3621e2cb1266bcd89f471813f')
 
 main_keyboard = telebot.types.ReplyKeyboardMarkup()
 main_keyboard.row('Привет')
@@ -28,7 +21,7 @@ main_keyboard.row("Хочу пообщаться")
 
 subjects_keyboard = telebot.types.ReplyKeyboardMarkup(True)
 subjects_keyboard.row('Английский', "Математика", "Русский")
-subjects_keyboard.row("Информатика")
+subjects_keyboard.row("Информатика", 'Литература')
 subjects_keyboard.row("Назад")
 
 tests_keyboard = telebot.types.ReplyKeyboardMarkup(True)
@@ -44,6 +37,12 @@ ru_keyboard.row('Переведи', "Озвучь")
 ru_keyboard.row("Проверь на орфографию")
 ru_keyboard.row("Назад")
 
+lit_keyboard = telebot.types.ReplyKeyboardMarkup(True)
+lit_keyboard.row('Найди сочинение')
+lit_keyboard.row("биография (кратко)")
+lit_keyboard.row("биография (полная)")
+lit_keyboard.row("Назад")
+
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -55,112 +54,128 @@ def name(message):
 
 
 def click_subjects_keyboard(message):
-    global en_tf, subjects_tf, ru_tf
     if message.text.lower() == 'английский':
         bot.send_message(message.chat.id, "Хорошо, пусть будет " + message.text.lower(), reply_markup=en_keyboard)
-        en_tf = True
-    elif en_tf:
-        en_subject(message, en_keyboard)
-    elif ru_tf:
-        ru_subject(message)
+        bot.register_next_step_handler(message, en_subject)
     elif message.text.lower() == 'русский':
         bot.send_message(message.chat.id, "Хорошо, пусть будет " + message.text.lower(), reply_markup=ru_keyboard)
-        ru_tf = True
+        bot.register_next_step_handler(message, ru_subject)
+    elif message.text.lower() == 'литература':
+        bot.send_message(message.chat.id, "Хорошо, пусть будет " + message.text.lower(), reply_markup=lit_keyboard)
+        bot.register_next_step_handler(message, lit_subject)
     elif message.text.lower() == "назад":
         bot.send_message(message.chat.id, "Вы вернулись назад", reply_markup=main_keyboard)
-        subjects_tf = False
+        bot.register_next_step_handler(message, send_message)
     else:
         bot.send_message(message.chat.id, "Я вас не понял", reply_markup=subjects_keyboard)
+        bot.register_next_step_handler(message, click_subjects_keyboard)
 
 
-def ru_subject(message):
-    global ru_tf
+def translate_word(message, keyboard, func):
+    bot.send_message(message.chat.id, subjects.translating(message.text, translate), reply_markup=keyboard)
+    bot.register_next_step_handler(message, func)
+
+
+def en_subject(message, keyboard=en_keyboard):
     if message.text.lower() == 'озвучь':
-        audio = True
         bot.send_message(message.chat.id, 'Введите текст и подождите')
-        en_subject(message)
+        bot.register_next_step_handler(message, audio, keyboard, 'en', en_subject)
     elif message.text.lower() == 'переведи':
-        translate_tf = True
         bot.send_message(message.chat.id,
                          'Введите с какого языка на какой и текст \n(Например, ru-en привет, en-ru hello)')
-
-
-def en_subject(message, keyboard):
-    global en_tf, audio, translate_tf
-    if audio:
-        audio = False
-        tts = gTTS(message.text, 'en')
-        tts.save('says.mp3')
-        file = open('says.mp3', 'rb')
-        bot.send_audio(message.chat.id, file, reply_markup=keyboard)
-    elif translate_tf:
-        translate_tf = False
-        try:
-            translate = YandexTranslate(
-                'trnsl.1.1.20191118T112631Z.f64a9d42a3ccc05f.2d0224570891cba3621e2cb1266bcd89f471813f')
-            x = message.text.split(' ')
-            a = x[0]
-            words = x[1:]
-            res_word = ''
-            for word in words:
-                res_word += translate.translate(word, a)['text'][0]
-                res_word += ' '
-            bot.send_message(message.chat.id, res_word, reply_markup=keyboard)
-        except:
-            bot.send_message(message.chat.id, 'Ошибка проверьте предыдущее сообщение', reply_markup=keyboard)
-    elif message.text.lower() == 'озвучь':
-        audio = True
-        bot.send_message(message.chat.id, 'Введите текст и подождите')
-        en_subject(message)
-    elif message.text.lower() == 'переведи':
-        translate_tf = True
-        bot.send_message(message.chat.id,
-                         'Введите с какого языка на какой и текст \n(Например, ru-en привет, en-ru hello)')
-
+        bot.register_next_step_handler(message, translate_word, en_keyboard, en_subject)
     elif message.text.lower() == 'назад':
-        en_tf = False
         bot.send_message(message.chat.id, "Вы вернулись назад", reply_markup=subjects_keyboard)
+        bot.register_next_step_handler(message, send_message)
     else:
         bot.send_message(message.chat.id, "Я вас не понял", reply_markup=en_keyboard)
+        bot.register_next_step_handler(message, en_subject)
+
+
+def audio(message, keyboard, lang, func):
+    bot.send_audio(message.chat.id, subjects.create_audio(lang, message), reply_markup=keyboard)
+    bot.register_next_step_handler(message, func)
+
+
+def orf(message, keyboard, lang, func):
+    bot.send_message(message.chat.id, subjects.check_orphographic(message.text, lang), reply_markup=keyboard)
+    bot.register_next_step_handler(message, func)
+
+
+def ru_subject(message, keyboard=ru_keyboard):
+    if message.text.lower() == 'озвучь':
+        bot.send_message(message.chat.id, 'Введите текст и подождите')
+        bot.register_next_step_handler(message, audio, keyboard, 'ru', ru_subject)
+    elif message.text.lower() == 'проверь на орфографию':
+        bot.send_message(message.chat.id, 'Введите текст и подождите')
+        bot.register_next_step_handler(message, orf, ru_keyboard, 'ru_RU', ru_subject)
+    elif message.text.lower() == 'переведи':
+        bot.send_message(message.chat.id,
+                         'Введите с какого языка на какой и текст \n(Например, ru-en привет, en-ru hello)')
+        bot.register_next_step_handler(message, translate_word, ru_keyboard, ru_subject)
+    elif message.text.lower() == 'назад':
+        bot.send_message(message.chat.id, "Вы вернулись назад", reply_markup=subjects_keyboard)
+        bot.register_next_step_handler(message, send_message)
+    else:
+        bot.send_message(message.chat.id, "Я вас не понял", reply_markup=keyboard)
+        bot.register_next_step_handler(message, ru_subject)
+
+
+def lit_bio_small(message):
+    bot.send_message(message.chat.id, subjects.lit_bio_search(message.text, True), reply_markup=lit_keyboard)
+    bot.register_next_step_handler(message, lit_subject)
+
+
+def lit_bio_long(message):
+    bot.send_message(message.chat.id, subjects.lit_bio_search(message.text), reply_markup=lit_keyboard)
+    bot.register_next_step_handler(message, lit_subject)
+
+
+def lit_work(message):
+    bot.register_next_step_handler(message, lit_subject)
+
+
+def lit_subject(message, keyboard=lit_keyboard):
+    if message.text.lower() == 'найди сочинение':
+        bot.send_message(message.chat.id, 'Введите название сочинения', reply_markup=keyboard)
+        bot.register_next_step_handler(message, lit_work)
+    elif message.text.lower() == 'биография (полная)':
+        bot.send_message(message.chat.id, 'Введите ваш запрос', reply_markup=keyboard)
+        bot.register_next_step_handler(message, lit_bio_long)
+    elif message.text.lower() == 'биография (кратко)':
+        bot.send_message(message.chat.id, 'Введите ваш запрос', reply_markup=keyboard)
+        bot.register_next_step_handler(message, lit_bio_small)
+    elif message.text.lower() == "назад":
+        bot.send_message(message.chat.id, "Вы вернулись назад", reply_markup=subjects_keyboard)
+        bot.register_next_step_handler(message, click_subjects_keyboard)
+    else:
+        bot.send_message(message.chat.id, "Я вас не понял", reply_markup=keyboard)
+        bot.register_next_step_handler(message, lit_subject)
+
+
+def get_dz(message):
+    if ' ' in message.text:
+        login, password = message.text.split(' ')
+        home_work.click_hm(login, password, message, bot, main_keyboard)
+    else:
+        bot.send_message(message.chat.id, 'Проверьте логин и пароль')
 
 
 @bot.message_handler(content_types=['text'])
 def send_message(message):
-    global gdz_tf, dz_tf, tests_tf, subjects_tf
-    if tests_tf:
-        tests_tf = False
-        # tests parse function
-        bot.send_message(message.chat.id, "вывод тестов", reply_markup=main_keyboard)
-    elif dz_tf:
-        dz_tf = False
-        if ' ' in message.text:
-            login, password = message.text.split(' ')
-            home_work.click_hm(login, password, message, bot, main_keyboard)
-        else:
-            bot.send_message(message.chat.id, 'Проверьте логин и пароль')
-    elif gdz_tf:
-        gdz_tf = False
-
-        bot.send_message(message.chat.id, "вывод гдз сайтов", reply_markup=main_keyboard)
-    elif subjects_tf:
-        click_subjects_keyboard(message)
-
-    elif message.text.lower() == "привет":
+    if message.text.lower() == "привет":
         bot.send_message(message.chat.id, "Привет " + name(message) + ".", reply_markup=main_keyboard)
     elif message.text.lower() == 'назад':
         bot.send_message(message.chat.id, 'Вы вернулись назад', reply_markup=main_keyboard)
     elif message.text.lower() == 'гдз':
-        gdz_tf = True
         bot.send_message(message.chat.id, 'Введите <название предмета> <класс> <автора>', reply_markup=main_keyboard)
     elif message.text.lower() == 'дз':
-        dz_tf = True
         bot.send_message(message.chat.id, 'Введите <логин> <пароль> от edu.tatar', reply_markup=main_keyboard)
+        bot.register_next_step_handler(message, get_dz)
     elif message.text.lower() == "предметы":
-        subjects_tf = True
         bot.send_message(message.chat.id, 'Вот список предметов', reply_markup=subjects_keyboard)
-
+        bot.register_next_step_handler(message, click_subjects_keyboard)
     elif message.text.lower() == "тесты":
-        tests_tf = True
         test_keyboard = tests_subjects.click_tests(message)
         bot.send_message(message.chat.id, 'Введите <название предмета> <класс>', reply_markup=test_keyboard)
     elif message.text.lower() == "хочу пообщаться":
--- subjects.py
+++ subjects.py
@@ -1,5 +1,59 @@
 import telebot
+from gtts import gTTS
+import enchant
+import wikipedia
 
 
-def create_audio(lang):
-    pass
+def create_audio(lang, message):
+    tts = gTTS(message.text, lang)
+    tts.save('says.mp3')
+    file = open('says.mp3', 'rb')
+    return file
+
+
+def translating(text, translate):
+    if ' ' in text:
+        x = text.split(' ')
+        a = x[0]
+        words = x[1:]
+        res_word = ''
+        for word in words:
+            res_word += translate.translate(word, a)['text'][0]
+            res_word += ' '
+        return res_word
+    return 'Ошибка проверьте предыдущее сообщение'
+
+
+def check_orphographic(text, lang="ru_RU"):
+    try:
+        d = enchant.Dict(lang)  # создание словаря для US English (англ язык)
+        words = text.split(' ')
+        if d.check(text) or (' ' in text and len([x for x in text.split(' ') if not d.check(x)]) == 0):
+            return 'Все правильно'  # проверка орфографии (верно)
+        res = []
+        for word in words:
+            if d.check(word):
+                res.append(word)
+            else:
+                res.append(word.upper())
+        return ' '.join(res)
+    except:
+        pass
+    return 'Ошибка проверьте предыдущее сообщение'
+
+
+def lit_bio_search(what, small=False):
+    try:
+        some = wikipedia.set_lang("ru")
+        some = wikipedia.page(what)
+        res = []
+        if small:
+            search = wikipedia.summary(sentences=10, title=what)  # sentenses - кол-во предложений
+            res.append(search)
+        else:
+            res.append(some.content)
+        res.append("\nИсточник: " + some.url)
+        return ' '.join(res)
+    except:
+        return 'Ошибка'
+
